3d32f5df4c12386189cc651945f24bcf39337402
Keith Vedananda <keith.vedananda@kaleido.com>
2019-04-11 23:23:52 -0400

    Add material-ui and TopBar

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
+import { TopBar } from './components/TopBar';
 import { NewExperimentPage } from './pages/NewExperimentPage';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <TopBar />
         <Switch>
           <Route path="/create/new" exact component={NewExperimentPage} />
           <Route render={() => <Redirect to="/create/new" />} />
--- ui/src/index.js
+++ ui/src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { store } from './store/store';
 import App from './App';
-import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+      <CssBaseline />
       <App />
     </BrowserRouter>
   </Provider>,
