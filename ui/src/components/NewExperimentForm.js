6873019f8f16ac2007ec92947f198ad1d90315c3
Keith Vedananda <keith.vedananda@kaleido.com>
2019-04-16 14:26:07 -0400

    Display request states in NewExperimentPage

---
+++ ui/src/components/NewExperimentForm.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export class NewExperimentForm extends Component {
+  render() {
+    return <div>New Experiment Form</div>;
+  }
+}
--- ui/src/pages/NewExperimentPage.js
+++ ui/src/pages/NewExperimentPage.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchEntityLists } from '../store/entityLists';
+import { NewExperimentForm } from '../components/NewExperimentForm';
 
 class NewExperimentPage extends Component {
   componentDidMount() {
     this.props.fetchEntityLists();
   }
+
   render() {
-    return <div>New Experiment</div>;
+    const { pending, loaded, error } = this.props;
+    if (pending) {
+      return <div>Loading...</div>;
+    } else if (error) {
+      return <div>{error}</div>;
+    } else if (loaded) {
+      return <NewExperimentForm />;
+    } else return <div />;
   }
 }
 
