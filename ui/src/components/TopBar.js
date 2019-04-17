3c7cde09f931ef90e342380a359422189044d0e8
Keith Vedananda <keith.vedananda@kaleido.com>
2019-04-17 00:23:07 -0400

    Remove material-ui

--- ui/src/components/TopBar.js
+++ ui/src/components/TopBar.js
@@ -1,54 +1,11 @@
 import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/Toolbar';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
+
 const logo = require('../images/kaleido_logo.png');
 
-const styles = theme => ({
-  appBar: {
-    position: 'relative',
-    boxShadow: 'none',
-    borderBottom: `1px solid ${theme.palette.grey['100']}`,
-    backgroundColor: 'white',
-  },
-  titleContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  title: {
-    marginLeft: theme.spacing.unit * 2,
-  },
-});
-
-class TopBar extends Component {
+export class TopBar extends Component {
   render() {
-    const { classes } = this.props;
     return (
-      <AppBar position="absolute" color="default" className={classes.appBar}>
-        <ToolBar>
-          <Grid container spacing={24} alignItems="baseline">
-            <Grid item xs={12}>
-              <div className={classes.titleContainer}>
-                <img width={12} src={logo} alt="" />
-                <Typography
-                  variant="h6"
-                  color="inherit"
-                  noWrap
-                  className={classes.title}
-                >
-                  Atlas
-                </Typography>
-              </div>
-            </Grid>
-          </Grid>
-        </ToolBar>
-      </AppBar>
+      <div><img width={12} src={logo} alt="" />Atlas</div>
     );
   }
 }
-
-const wrapped = withStyles(styles)(TopBar);
-export { wrapped as TopBar };
--- ui/src/index.js
+++ ui/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { store } from './store/store';
 import App from './App';
@@ -10,7 +9,6 @@ import App from './App';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <CssBaseline />
       <App />
     </BrowserRouter>
   </Provider>,
