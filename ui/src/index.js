a68019d446fcc61849df78ec2e9fa001aa2c9c87
Keith Vedananda <keith.vedananda@kaleido.com>
2019-04-17 00:38:23 -0400

    Add react-semantic-ui

--- ui/src/index.js
+++ ui/src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { store } from './store/store';
 import App from './App';
 
+import 'semantic-ui-css/semantic.min.css';
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
--- ui/src/pages/NewExperimentPage.js
+++ ui/src/pages/NewExperimentPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Grid } from 'semantic-ui-react';
 
 import { fetchEntityLists } from '../store/entityLists';
 import { NewExperimentForm } from '../components/NewExperimentForm';
@@ -16,7 +17,13 @@ class NewExperimentPage extends Component {
     } else if (error) {
       return <div>{error}</div>;
     } else if (loaded) {
-      return <NewExperimentForm />;
+      return (
+        <Grid centered columns={2}>
+          <Grid.Column>
+            <NewExperimentForm />
+          </Grid.Column>
+        </Grid>
+      );
     } else return <div />;
   }
 }
