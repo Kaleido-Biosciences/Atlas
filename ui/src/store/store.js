04b7fdc0e391fde80f3a7c64020cfdd1d2c60f06
Keith Vedananda <keith.vedananda@kaleido.com>
2019-04-19 16:09:30 -0400

    Only use redux-logger in development mode

--- ui/src/store/store.js
+++ ui/src/store/store.js
@@ -5,11 +5,16 @@ import { reducer as formReducer } from 'redux-form';
 import { entityListsReducer } from './entityLists';
 import { createExperimentReducer } from './createExperiment';
 
+const middleware = getDefaultMiddleware();
+if (process.env.NODE_ENV === `development`) {
+  middleware.push(logger);
+}
+
 export const store = configureStore({
   reducer: {
     entityLists: entityListsReducer,
     createExperiment: createExperimentReducer,
     form: formReducer,
   },
-  middleware: [...getDefaultMiddleware(), logger],
+  middleware,
 });
