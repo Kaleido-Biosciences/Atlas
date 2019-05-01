0f7afcf215827afb609444372129fbdcbe6172eb
Keith Vedananda <keith.vedananda@kaleido.com>
2019-05-01 02:38:21 -0400

    Remove unused Icon import

--- ui/src/components/PlateMap.js
+++ ui/src/components/PlateMap.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Icon } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 
 import { plateMapRowHeaders } from '../constants';
 import { Well } from './Well';
