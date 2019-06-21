2bf4de2b340ca7332f4f0c1d7a80f42904b7d54c
Keith Vedananda <keith.vedananda@kaleido.com>
2019-06-21 17:32:20 -0400

    Move Provider and BrowserRouter into App - Test was failing since they were in index and the test doesnt nclude the router

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
+import { store } from './store/store';
 import { Header } from './components/Header';
 import { CreateNew } from './pages/create/CreateNew';
 const logo = require('./images/kaleido_logo.png');
@@ -8,15 +10,19 @@ const logo = require('./images/kaleido_logo.png');
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Header logo={logo} />
-        <div className="main-container">
-          <Switch>
-            <Route path="/create" component={CreateNew} />
-            <Route render={() => <Redirect to="/create/select" />} />
-          </Switch>
-        </div>
-      </React.Fragment>
+      <Provider store={store}>
+        <BrowserRouter>
+          <React.Fragment>
+            <Header logo={logo} />
+            <div className="main-container">
+              <Switch>
+                <Route path="/create" component={CreateNew} />
+                <Route render={() => <Redirect to="/create/select" />} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 
-import { store } from './store/store';
 import App from './App';
 
 import 'semantic-ui-css/semantic.min.css';
 import './styles/styles.css';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
