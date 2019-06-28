d074ee8bc11a4370948d8bca8713e052666a7170
Keith Vedananda <keith.vedananda@kaleido.com>
2019-06-28 15:58:25 -0400

    Add experiment search

---
+++ ui/src/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+import { API_URL } from './config';
+
+export function fetchExperiments(page, size, nameContains, descContains) {
+  let queryString = '';
+  if (page || size || nameContains || descContains) {
+    const params = [];
+    if (page) params.push(`page=${page}`);
+    if (size) params.push(`size=${size}`);
+    if (nameContains) params.push(`name.contains=${nameContains}`);
+    if (descContains) params.push(`description.contains=${descContains}`);
+    queryString += '?' + params.join('&');
+  }
+  return axios.get(API_URL + '/experiments' + queryString);
+}
--- ui/src/pages/create/SelectStep.js
+++ ui/src/pages/create/SelectStep.js
@@ -1,12 +1,68 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Search } from 'semantic-ui-react';
+import _ from 'lodash';
 
+import { fetchExperiments } from '../../api';
 import './SelectStep.css';
 
 class SelectStep extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      results: null,
+      showNoResults: false,
+      noResultsMessage: 'No results found.',
+    };
+  }
+
+  handleSearchChange = (e, { value }) => {
+    this.setState({ loading: true, results: [], showNoResults: false });
+    fetchExperiments(0, 5, value)
+      .then(response => {
+        const results = response.data.map(exp => {
+          return { title: exp.name, description: exp.description, data: exp };
+        });
+        this.setState({
+          loading: false,
+          results,
+          showNoResults: true,
+          noResultsMessage: 'No results found.',
+        });
+      })
+      .catch(err => {
+        this.setState({
+          loading: false,
+          showNoResults: true,
+          noResultsMessage: 'Error occurred while searching.',
+        });
+      });
+  };
+
+  handleResultSelect = (e, { result }) => {
+    console.log(result.data);
+  };
+
   render() {
-    return <div className="select-step-container">Select step</div>;
+    const { loading, results, showNoResults, noResultsMessage } = this.state;
+    return (
+      <div className="select-step-container">
+        <div className="select-step-centered">
+          <Search
+            fluid
+            input={{ fluid: true }}
+            loading={loading}
+            results={results}
+            onSearchChange={_.debounce(this.handleSearchChange, 500)}
+            onResultSelect={this.handleResultSelect}
+            showNoResults={showNoResults}
+            noResultsMessage={noResultsMessage}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
