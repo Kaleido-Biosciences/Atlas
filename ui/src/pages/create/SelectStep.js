866d13ace791d60114b4e1bd238aa6a9dabdb8e2
Keith Vedananda <keith.vedananda@kaleido.com>
2019-06-30 19:40:42 -0400

    Select Step CSS module

--- ui/src/pages/create/SelectStep.js
+++ ui/src/pages/create/SelectStep.js
@@ -6,7 +6,19 @@ import classNames from 'classnames';
 import { ExperimentSearch } from '../../components/ExperimentSearch';
 import { ExperimentCard } from '../../components/ExperimentCard';
 import { PlateSizeForm } from '../../components/PlateSizeForm';
-import './SelectStep.css';
+import styles from './SelectStep.module.css';
+
+const renderHeader = options => {
+  const { stepClass, complete, stepNumber, headerText } = options;
+  return (
+    <div className={styles.headerContainer}>
+      <div className={stepClass}>
+        {complete ? <Icon name="check" /> : stepNumber}
+      </div>
+      <Header as="h3">{headerText}</Header>
+    </div>
+  );
+};
 
 class SelectStep extends Component {
   state = {
@@ -26,35 +38,33 @@ class SelectStep extends Component {
     const { experiment, plateSize } = this.state;
     const experimentComplete = experiment;
     const plateSizeComplete = plateSize && plateSize.rows && plateSize.columns;
-    const experimentStepClass = classNames({
-      'select-step-header-step': true,
-      completed: experimentComplete,
+    const experimentStepClass = classNames(styles.headerStep, {
+      [styles.completed]: experimentComplete,
     });
-    const plateSizeStepClass = classNames({
-      'select-step-header-step': true,
-      completed: plateSizeComplete,
+    const plateSizeStepClass = classNames(styles.headerStep, {
+      [styles.completed]: plateSizeComplete,
     });
     return (
-      <div className="select-step-container">
-        <div className="select-step-centered">
-          <div className="select-step-experiment">
-            <div className="select-step-header-container">
-              <div className={experimentStepClass}>
-                {experimentComplete ? <Icon name="check" /> : '1'}
-              </div>
-              <Header as="h3">Select an Experiment</Header>
-            </div>
+      <div className={styles.container}>
+        <div className={styles.columns}>
+          <div className={styles.experimentSearch}>
+            {renderHeader({
+              stepClass: experimentStepClass,
+              complete: experimentComplete,
+              stepNumber: '1',
+              headerText: 'Select an Experiment',
+            })}
             <ExperimentSearch onSelect={this.handleExperimentSelect} />
             {experiment && <ExperimentCard experiment={experiment} />}
           </div>
-          <div className="select-step-plate-size-container">
-            <div className="select-step-plate-size">
-              <div className="select-step-header-container">
-                <div className={plateSizeStepClass}>
-                  {plateSizeComplete ? <Icon name="check" /> : '2'}
-                </div>
-                <Header as="h3">Select Plate Size</Header>
-              </div>
+          <div className={styles.plateSizeFormContainer}>
+            <div>
+              {renderHeader({
+                stepClass: plateSizeStepClass,
+                complete: plateSizeComplete,
+                stepNumber: '2',
+                headerText: 'Select Plate Size',
+              })}
               <PlateSizeForm onChange={this.handlePlateSizeChange} />
             </div>
           </div>
