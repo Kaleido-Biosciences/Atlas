8dec1fa0371625d87f361c269e6efdaf5c5cae10
Keith Vedananda <keith.vedananda@kaleido.com>
2019-07-28 19:02:35 -0400

    Connect the print step and add a stylesheet

--- ui/src/pages/create/PrintStep.js
+++ ui/src/pages/create/PrintStep.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export class PrintStep extends Component {
+import styles from './PrintStep.module.css';
+
+class PrintStep extends Component {
   render() {
-    return <div>Print plates here</div>;
+    const { experiment, plateMaps } = this.props;
+    return (
+      <div className={styles.printStep}>
+        <h4>Experiment</h4>
+        <div>{JSON.stringify(experiment)}</div>
+        <h4>Plate Maps</h4>
+        <div>{JSON.stringify(plateMaps)}</div>
+      </div>
+    );
   }
 }
+
+const mapState = (state, props) => {
+  const { experiment, plateMaps } = state.createExperiment;
+  return {
+    experiment,
+    plateMaps,
+  };
+};
+
+const connected = connect(mapState)(PrintStep);
+export { connected as PrintStep };
