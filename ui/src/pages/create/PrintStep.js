ed2fb52dbb2486b7263020d56fc2b0281cb55191
Keith Vedananda <keith.vedananda@kaleido.com>
2019-07-31 23:15:18 -0400

    Set the last step status complete on print

--- ui/src/pages/create/PrintStep.js
+++ ui/src/pages/create/PrintStep.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import ReactToPrint from 'react-to-print';
 import { Button, Icon } from 'semantic-ui-react';
 
+import { setStepThreeComplete } from '../../store/experimentActions';
 import { Printout } from '../../components/Printout/Printout';
 import styles from './PrintStep.module.css';
 
@@ -18,6 +19,7 @@ class PrintStep extends Component {
               Print
             </Button>
           )}
+          onBeforePrint={this.props.onPrint}
           content={() => this.componentRef}
         />
         <Printout
@@ -38,5 +40,12 @@ const mapState = (state, props) => {
   };
 };
 
-const connected = connect(mapState)(PrintStep);
+const mapDispatch = {
+  onPrint: setStepThreeComplete,
+};
+
+const connected = connect(
+  mapState,
+  mapDispatch
+)(PrintStep);
 export { connected as PrintStep };
