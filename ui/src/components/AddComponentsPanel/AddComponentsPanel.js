83ad55651508f64c4e4594cc13ad9ad7167565fa
Keith Vedananda <keith.vedananda@kaleido.com>
2019-08-29 18:53:44 -0400

    Move modal contents to panel

--- ui/src/components/AddComponentsPanel/AddComponentsPanel.js
+++ ui/src/components/AddComponentsPanel/AddComponentsPanel.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { Tab } from 'semantic-ui-react';
+
+import { ImportComponents } from './ImportComponents';
+import styles from './AddComponentsPanel.module.css';
+
+const panes = [
+  {
+    menuItem: 'Import',
+    render: () => (
+      <Tab.Pane attached={false}>
+        <ImportComponents />
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: 'Search',
+    render: () => <Tab.Pane attached={false}>Search here.</Tab.Pane>,
+  },
+];
 
 export class AddComponentsPanel extends Component {
   render() {
-    return <div>Add Components Panel</div>;
+    return (
+      <div className={styles.addComponentsPanel}>
+        <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
+      </div>
+    );
   }
 }
