59266461012bc317d040eda2be0c947d015465dd
dflemming <daisy.flemming@kaleido.com>
2019-09-04 00:18:28 -0400

    save platemap using new schema

--- ui/src/api/aws.js
+++ ui/src/api/aws.js
@@ -15,6 +15,7 @@ AWS.config.update({
 
 let docClient = new AWS.DynamoDB.DocumentClient();
 let table = DYNAMODB_TABLE;
+let completedStatus = 'COMPLETED';
 
 export function fetchPlates(experimentId, status) {
   return new Promise((resolve, reject) => {
@@ -23,16 +24,15 @@ export function fetchPlates(experimentId, status) {
       TableName: table,
       KeyConditionExpression: '#e = :eeee and #s = :ssss',
       ExpressionAttributeNames: {
-        '#e': 'experiment',
-        '#s': 'status',
+        '#e': 'experiment_status',
+        '#s': 'version',
       },
       ExpressionAttributeValues: {
-        ':eeee': experimentId,
-        ':ssss': status,
+        ':eeee': experimentId+"_"+status,
+        ':ssss': 0,
       },
       ScanIndexForward: false,
       ConsistentRead: false,
-      Limit: 1,
     };
 
     docClient.query(params, function(err, data) {
@@ -40,7 +40,7 @@ export function fetchPlates(experimentId, status) {
         reject(err);
       } else {
         if (data.Items.length > 0) {
-          plateMaps = JSON.parse(data.Items[0].plateMaps);
+          plateMaps = JSON.parse(data.Items[data.Items.length - 1].plateMaps);
         }
         resolve(plateMaps);
       }
@@ -51,29 +51,63 @@ export function fetchPlates(experimentId, status) {
 export function saveExperimentPlates(experimentName, status, plateMaps) {
   return new Promise((resolve, reject) => {
     let plateMapsToSave = JSON.stringify(plateMaps);
+    let version = 0;
+    if (status === completedStatus){
+      var p1 = readCompletedItem(experimentName);
+      p1.then(function(count){
+        saveToDB(experimentName, status,count + 1, plateMapsToSave, reject, resolve);
+      });
+    }
+    else{
+      saveToDB(experimentName, status, version, plateMapsToSave, reject, resolve);
+    }
+  });
+}
 
-    let params = {
-      TableName: table,
-      Key: {
-        experiment: experimentName,
-        status: status,
-      },
-      UpdateExpression: 'set plateMaps=:p',
-      ExpressionAttributeValues: {
-        ':p': plateMapsToSave,
-      },
-      ReturnValues: 'UPDATED_NEW',
-    };
+function saveToDB(experimentName, status, version, plateMapsToSave, reject, resolve) {
+  let params = {
+    TableName: table,
+    Key: {
+      experiment_status: experimentName + "_" + status,
+      version: version,
+    },
+    UpdateExpression: 'set plateMaps=:p',
+    ExpressionAttributeValues: {
+      ':p': plateMapsToSave,
+    },
+    ReturnValues: 'UPDATED_NEW',
+  };
 
-    docClient.update(params, function(err, data) {
-      if (err) {
-        reject(err);
-        console.error(
+  docClient.update(params, function (err, data) {
+    if (err) {
+      reject(err);
+      console.error(
           'Unable to update item. Error JSON:',
           JSON.stringify(err, null, 2)
-        );
+      );
+    } else {
+      resolve({data});
+    }
+  });
+}
+
+function readCompletedItem(experimentName) {
+  return new Promise(function(resolve, reject) {
+    var params = {
+      TableName : table,
+      KeyConditionExpression: "#e = :eeee",
+      ExpressionAttributeNames:{
+        "#e": "experiment_status"
+      },
+      ExpressionAttributeValues: {
+        ":eeee": experimentName+"_"+completedStatus
+      }
+    };
+    docClient.query(params, function (err, data) {
+      if (err) {
+        resolve(0);
       } else {
-        resolve({ data });
+        resolve(data['Count']);
       }
     });
   });
