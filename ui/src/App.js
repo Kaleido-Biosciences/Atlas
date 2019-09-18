186f4f501ee3f4ad230bdca8e8b32df98b68ccb2
Keith Vedananda <keith.vedananda@kaleido.com>
2019-09-17 22:45:56 -0400

    Add link to header and move logo

--- ui/src/App.js
+++ ui/src/App.js
@@ -7,7 +7,6 @@ import { Header } from './components/Header/Header';
 import { CreateNew } from './pages/create/CreateNew';
 import styles from './App.module.css';
 import './styles.css';
-const logo = require('./images/kaleido_logo.png');
 
 class App extends Component {
   render() {
@@ -15,7 +14,7 @@ class App extends Component {
       <Provider store={store}>
         <BrowserRouter>
           <React.Fragment>
-            <Header logo={logo} />
+            <Header />
             <div className={styles.mainContainer}>
               <Switch>
                 <Route path="/create" component={CreateNew} />
