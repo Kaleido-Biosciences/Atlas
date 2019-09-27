7e899b090f4f318546598b60dfa10759539fdb64
dflemming <daisy.flemming@kaleido.com>
2019-09-27 09:47:07 -0400

    merge develop over

--- ui/src/components/PlateSidebar/ComponentList/AddAttributeComponent.js
+++ ui/src/components/PlateSidebar/ComponentList/AddAttributeComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap'
 import { Grid, Icon, Input, Segment, Select } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
@@ -31,10 +32,10 @@ export class AddAttributeComponent extends Component {
 
   setValue = (event, data) => {
     if (data.value && this.state.value_type === 'Integer' && Number.isNaN(parseInt(data.value))){
-      alert('are you kidding me? value not an integer');
+      alert('Value is not an integer.');
     }
     else if (data.value && this.state.value_type === 'Float' && Number.isNaN(parseFloat(data.value))){
-      alert('are you kidding me? value not a float');
+      alert('Value is not numeric.');
     }
     else {
       this.setState({ value: data.value });
@@ -49,9 +50,9 @@ export class AddAttributeComponent extends Component {
 
   handleAddClick = (event) => {
     const { key, value, value_type, value_unit } = this.state;
-    let id = (key + '_' + value).replace(/ /g, '_');
-    let unit = value_unit ? value_unit : '';
-    let displayName = key + ": " + value + unit;
+    let id = value ? (key + '_' + value).replace(/ /g, '_') : key;
+    let unit = value ? (value_unit ? value_unit : '' ): '';
+    let displayName = value ? key + ": " + value + unit : key;
     let component = {
       id: 'ATTRIBUTE_' + id,
       type: "attribute",
@@ -61,6 +62,7 @@ export class AddAttributeComponent extends Component {
       data: { id: id, name: displayName, key: key, value: value, value_type: value_type, value_unit: value_unit }
     };
     this.props.onAddClick({ component });
+    this.setState({ key: null, value: null, value_type: 'String', value_unit: null });
     event.preventDefault();
   };
 
@@ -69,9 +71,7 @@ export class AddAttributeComponent extends Component {
       return (
         <Grid.Row>
           {this.renderValueComponent()}
-          <Grid.Column width={6}>
-            <Input fluid size='small' title='value_unit' placeholder='Unit' onChange={this.setValueUnit}/>
-          </Grid.Column>
+          {this.renderValueUnitComponent()}
           <Grid.Column width={4}>
             <Icon title='Add Attribute' link color='blue' size='large' name='plus circle'
                   onClick={this.handleAddClick}/>
@@ -80,6 +80,17 @@ export class AddAttributeComponent extends Component {
       );
     } else {
       return "";
+    }
+  };
+
+  renderValueUnitComponent = () => {
+    const {value_type} = this.state;
+    if (value_type !== 'Boolean'){
+      return (
+        <Grid.Column width={6}>
+          <Input fluid size='small' title='value_unit' placeholder='Unit' onChange={this.setValueUnit}/>
+        </Grid.Column>
+      )
     }
   };
 
@@ -121,7 +132,7 @@ export class AddAttributeComponent extends Component {
         <Grid>
           <Grid.Row>
             <Grid.Column width={6}>
-              <Input fluid size='small' title='key' placeholder='Name' onChange={this.setKey}/>
+              <Input fluid size='small' title='key' placeholder='Name' onChange={this.setKey} />
             </Grid.Column>
             <Grid.Column width={10}>
               <Select
