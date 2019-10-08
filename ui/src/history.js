a24bb0aa04f018f71d335678ec088b31f327a5ed
dflemming <daisy.flemming@kaleido.com>
2019-10-08 12:09:32 -0400

    merge with devlop

--- ui/src/history.js
+++ ui/src/history.js
@@ -1,4 +1,4 @@
-const createHistory = require("history").createBrowserHistory;
+const createHistory = require("ui/src/history").createBrowserHistory;
 
 export default createHistory({
   basename: process.env.NODE_ENV === 'development' ? '' : '/'
