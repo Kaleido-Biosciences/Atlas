b320fd6500fafa046bfb4529ddfe4aa226735ebb
kvedananda <keith.vedananda@kaleido.com>
2019-11-25 19:11:33 -0500

    Add version to experiment details

--- ui/src/components/experiment/ExperimentDetails/ExperimentDetails.js
+++ ui/src/components/experiment/ExperimentDetails/ExperimentDetails.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Loader } from 'semantic-ui-react';
+import { Loader, Card } from 'semantic-ui-react';
+import moment from 'moment';
 
 import {
   REQUEST_PENDING,
   REQUEST_SUCCESS,
   REQUEST_ERROR,
+  STATUS_COMPLETED,
 } from '../../../constants';
 import { fetchExperimentVersions } from '../../../store/experimentActions';
 import styles from './ExperimentDetails.module.css';
+
+const ldapToJS = n => {
+  return new Date(n / 1e4 - 1.16444736e13);
+};
 
 class ExperimentDetails extends Component {
   componentDidMount() {
@@ -18,10 +24,34 @@ class ExperimentDetails extends Component {
       this.props.onMount(name);
     }
   }
+  renderVersions(versions) {
+    return versions.map(version => {
+      const status = version.experiment_status.split('_')[1];
+      const count = version.plateMaps.length;
+      const key = version.version;
+      let displayTime = '-';
+      if (status === STATUS_COMPLETED) {
+        const jsTime = ldapToJS(version.version);
+        const saveTime = moment(jsTime);
+        displayTime = saveTime.format('MMMM Do YYYY, h:mm a');
+      }
+      return (
+        <Card link key={key}>
+          <Card.Content>
+            <Card.Header>{status}</Card.Header>
+            <Card.Meta>
+              <span>{displayTime}</span>
+            </Card.Meta>
+            <Card.Description>{count} plates</Card.Description>
+          </Card.Content>
+        </Card>
+      );
+    });
+  }
   render() {
-    const { experiment, versionsLoadingStatus, versions } = this.props;
+    const { versions, versionsLoadingStatus } = this.props;
     return (
-      <div>
+      <div className={styles.experimentDetails}>
         {versionsLoadingStatus === REQUEST_PENDING && (
           <div className={styles.loader}>
             <Loader active inline="centered">
@@ -33,9 +63,8 @@ class ExperimentDetails extends Component {
           <div>An error occurred while retrieving versions</div>
         )}
         {versionsLoadingStatus === REQUEST_SUCCESS && (
-          <div>{versions.length}</div>
+          <Card.Group>{this.renderVersions(versions)}</Card.Group>
         )}
-        {experiment.name} {experiment.description}
       </div>
     );
   }
@@ -43,6 +72,7 @@ class ExperimentDetails extends Component {
 
 ExperimentDetails.propTypes = {
   experiment: PropTypes.object,
+  versions: PropTypes.array,
   versionsLoadingStatus: PropTypes.string,
   onMount: PropTypes.func,
 };
