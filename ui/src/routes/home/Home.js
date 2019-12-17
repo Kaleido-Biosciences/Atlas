b7e266d19380796ce211ea71f8d136e88e521a42
kvedananda <keith.vedananda@kaleido.com>
2019-12-17 15:28:30 -0500

    Remove connect and setting activity in Home

--- ui/src/routes/home/Home.js
+++ ui/src/routes/home/Home.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
 import { ActivitySearch } from '../../components/activity/ActivitySearch';
-import { setCurrentActivity } from '../../store/activitiesActions';
 import styles from './Home.module.css';
 
-class Home extends Component {
+export class Home extends Component {
   handleSelect = ({ activity }) => {
-    if (this.props.onSelect) {
-      this.props.onSelect({ activity });
-    }
     this.props.history.push(`/activities/${activity.id}`);
   };
 
@@ -22,14 +17,3 @@ class Home extends Component {
     );
   }
 }
-
-const mapState = (state, props) => {
-  return {};
-};
-
-const mapDispatch = {
-  onSelect: setCurrentActivity,
-};
-
-const connected = connect(mapState, mapDispatch)(Home);
-export { connected as Home };
