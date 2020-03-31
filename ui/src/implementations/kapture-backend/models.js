65cdd0b1bf0b5e0a74e09ca72bf7b41fab2b7a89
kvedananda <keith.vedananda@kaleido.com>
2020-03-31 00:47:02 -0400

    Remove unnecessary functions from models

--- ui/src/implementations/kapture-backend/models.js
+++ ui/src/implementations/kapture-backend/models.js
@@ -3,58 +3,6 @@ import {
   importContainer,
   importContainerGrid,
 } from './containerFunctions';
-import { createComponent } from '../../models';
-import { DEFAULT_COMPONENT_COLOR_CODES } from '../../constants';
-
-const createEditorComponentFromKaptureData = (
-  kaptureData,
-  type,
-  timepoints
-) => {
-  const id = `${type.toUpperCase()}_${kaptureData.id}`;
-  const displayName = getDisplayName(kaptureData);
-  const description = getDescription(timepoints);
-  const color = DEFAULT_COMPONENT_COLOR_CODES[type];
-  const options = {
-    timepoints: timepoints.map(timepoint => {
-      return Object.assign({}, timepoint);
-    }),
-  };
-  return createComponent(
-    id,
-    type,
-    displayName,
-    description,
-    options,
-    null,
-    color,
-    kaptureData
-  );
-};
-
-function getDisplayName(data) {
-  let displayName = data.name;
-  if (data.alias) {
-    //For communities
-    displayName += ` : (${data.alias})`;
-  } else if (data.aliases && data.aliases.length > 0) {
-    //This is for compounds
-    data.aliases.forEach(
-      aliasElement => (displayName += ` : (${aliasElement.alias})`)
-    );
-  }
-  return displayName;
-}
-
-function getDescription(timepoints) {
-  const timepointStrings = timepoints.map(timepoint => {
-    if (timepoint.concentration) {
-      return `${timepoint.concentration.toFixed(2)} @ 
-            ${timepoint.time}h`;
-    } else return '';
-  });
-  return timepointStrings.join(', ');
-}
 
 export {
   createComponent,
@@ -65,9 +13,4 @@ export {
   createContainersForGrid,
   addContainersToGrid,
 } from '../../models';
-export {
-  createEditorComponentFromKaptureData,
-  exportContainers,
-  importContainer,
-  importContainerGrid,
-};
+export { exportContainers, importContainer, importContainerGrid };
