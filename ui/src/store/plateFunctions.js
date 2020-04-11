61a5c59fd238abbbe7f18a101683b2f6f46e32a0
kvedananda <keith.vedananda@kaleido.com>
2020-04-10 21:55:43 -0400

    Remove unused plate functions

--- ui/src/store/plateFunctions.js
+++ ui/src/store/plateFunctions.js
@@ -1,5 +1,4 @@
 import {
-  PLATE_ROW_HEADERS,
   DEFAULT_TIMEPOINT_TIME,
   DEFAULT_TIMEPOINT_CONCENTRATION,
   DEFAULT_TIMEPOINT_COMMUNITY_CONCENTRATION,
@@ -10,143 +9,6 @@ import {
   COMPONENT_TYPE_SUPPLEMENT,
   COMPONENT_TYPE_ATTRIBUTE,
 } from '../constants';
-
-// import { api } from '../api';
-// const {
-//   fetchCommunity,
-//   fetchCompound,
-//   fetchMedium,
-//   fetchSupplement,
-// } = api.kapture;
-
-export function getActivePlate(plates) {
-  if (plates.length > 0) {
-    return plates.find(plate => plate.active);
-  } else return null;
-}
-
-export function getSelectedWells(plate) {
-  const wells = plate.wells.flat();
-  return wells.filter(well => well.selected);
-}
-
-export function applySelectedComponentsToWells(plate, wellIds, components) {
-  const wells = plate.wells.flat();
-  const filteredWells = wells.filter(well => {
-    return wellIds.includes(well.id);
-  });
-  const updatedWells = [];
-  filteredWells.forEach(well => {
-    components.forEach(component => {
-      if (component.selected) {
-        const existingComponent = well.components.find(
-          comp => comp.id === component.id
-        );
-        const { selected, editing, ...wellComponent } = component;
-        if (!existingComponent) {
-          well.components.push(wellComponent);
-        } else {
-          const existingTimepoints = existingComponent.timepoints;
-          if (wellComponent.timepoints) {
-            wellComponent.timepoints.forEach(newTimepoint => {
-              const index = existingTimepoints.findIndex(
-                eTimepoint => eTimepoint.time === newTimepoint.time
-              );
-              if (index > -1) {
-                existingTimepoints.splice(index, 1, newTimepoint);
-              } else {
-                existingTimepoints.push(newTimepoint);
-              }
-            });
-          }
-        }
-      }
-    });
-    updatedWells.push(well);
-  });
-  return updatedWells;
-}
-
-export function applyComponentsToWells(plate, wellIds, components) {
-  const wells = plate.wells.flat();
-  const filteredWells = wells.filter(well => {
-    return wellIds.includes(well.id);
-  });
-  const updatedWells = [];
-  filteredWells.forEach(well => {
-    components.forEach(component => {
-      const existingComponent = well.components.find(
-        comp => comp.id === component.id
-      );
-      const { selected, editing, ...wellComponent } = component;
-      if (!existingComponent) {
-        well.components.push(wellComponent);
-      } else {
-        const existingTimepoints = existingComponent.timepoints;
-        if (wellComponent.timepoints) {
-          wellComponent.timepoints.forEach(newTimepoint => {
-            const index = existingTimepoints.findIndex(
-              eTimepoint => eTimepoint.time === newTimepoint.time
-            );
-            if (index > -1) {
-              existingTimepoints.splice(index, 1, newTimepoint);
-            } else {
-              existingTimepoints.push(newTimepoint);
-            }
-          });
-        }
-      }
-    });
-    updatedWells.push(well);
-  });
-  return updatedWells;
-}
-
-export function findPlateById(id, plates) {
-  return plates.find((plate, i) => {
-    return plate.id === id;
-  });
-}
-
-export function createWell(id, name, index, components = []) {
-  return {
-    id,
-    name,
-    index,
-    components,
-    selected: false,
-  };
-}
-
-export function createPlate(wells, id, barcode) {
-  return {
-    active: false,
-    wells,
-    barcode: barcode || null,
-    id: id || null,
-  };
-}
-
-export function createPlateWithDimensions(dimensions) {
-  return createPlate(createPlateWells(dimensions));
-}
-
-export function createPlateWells(dimensions) {
-  const { rows, columns } = dimensions;
-  const wells = [];
-  let wellCount = 0;
-  for (let i = 0; i < rows; i++) {
-    const row = [];
-    const rowLetter = PLATE_ROW_HEADERS[i];
-    for (let i = 0; i < columns; i++) {
-      const id = `${rowLetter}${i + 1}`;
-      row.push(createWell(id, id, wellCount));
-      wellCount++;
-    }
-    wells.push(row);
-  }
-  return wells;
-}
 
 export function createComponent(data, type) {
   const displayName = getDisplayName(data);
@@ -176,7 +38,7 @@ export function getDisplayName(data) {
   } else if (data.aliases && data.aliases.length > 0) {
     //This is for compounds
     data.aliases.forEach(
-      aliasElement => (displayName += ` : (${aliasElement.alias})`)
+      (aliasElement) => (displayName += ` : (${aliasElement.alias})`)
     );
   }
   return displayName;
@@ -201,147 +63,15 @@ export function createTimepoint(
   return { time, concentration };
 }
 
-export function exportPlates(plates) {
-  return plates.map((plate, i) => {
-    return {
-      id: i + 1,
-      barcode: plate.barcode,
-      data: plate.wells.map(row => {
-        return row.map(col => {
-          const well = col;
-          return {
-            id: well.id,
-            components: well.components.map(component => {
-              if (component.type === 'attribute') {
-                return {
-                  type: component.type,
-                  id: component.data.id,
-                  attributeValues: component.data,
-                };
-              } else {
-                return {
-                  type: component.type,
-                  id: component.data.id,
-                  timepoints: component.timepoints,
-                };
-              }
-            }),
-          };
-        });
-      }),
-    };
-  });
-}
-
-// export async function importPlates(plates) {
-//   if (plates) {
-//     const components = await fetchComponentsForPlates(plates);
-//     const statePlates = plates.map(plate => {
-//       let wellIndex = 0;
-//       const stateWells = plate.data.map(rows => {
-//         return rows.map(well => {
-//           const stateComponents = well.components.map(component => {
-//             const lookup = components[component.type];
-//             const data = lookup.find(data => data.id === component.id);
-//             const stateComponent = createComponent(data, component.type);
-//             stateComponent.timepoints = component.timepoints;
-//             return stateComponent;
-//           });
-//           const stateWell = createWell(
-//             well.id,
-//             well.id,
-//             wellIndex,
-//             stateComponents
-//           );
-//           wellIndex++;
-//           return stateWell;
-//         });
-//       });
-//       return createPlate(stateWells, plate.id, plate.barcode);
-//     });
-//     if (statePlates.length) {
-//       statePlates[0].active = true;
-//     }
-//     return statePlates;
-//   } else return null;
-// }
-
-// async function fetchComponentsForPlates(plates) {
-//   const components = {
-//     community: [],
-//     compound: [],
-//     medium: [],
-//     supplement: [],
-//     attribute: [],
-//   };
-//   const response = {
-//     community: [],
-//     compound: [],
-//     medium: [],
-//     supplement: [],
-//     attribute: [],
-//   };
-//   plates.forEach(plate => {
-//     const wells = plate.data.flat();
-//     wells.forEach(well => {
-//       well.components.forEach(component => {
-//         const cType = component.type;
-//         if (cType === 'attribute') {
-//           response.attribute.push(component.attributeValues);
-//         } else if (!components[cType].includes(component.id)) {
-//           components[cType].push(component.id);
-//         }
-//       });
-//     });
-//   });
-//   let promises, results;
-//   promises = components.community.map(id => {
-//     return fetchCommunity(id);
-//   });
-//   results = await Promise.all(promises);
-//   results.forEach(result => {
-//     response.community.push(result.data);
-//   });
-//   promises = components.compound.map(id => {
-//     return fetchCompound(id);
-//   });
-//   results = await Promise.all(promises);
-//   results.forEach(result => {
-//     response.compound.push(result.data);
-//   });
-//   promises = components.medium.map(id => {
-//     return fetchMedium(id);
-//   });
-//   results = await Promise.all(promises);
-//   results.forEach(result => {
-//     response.medium.push(result.data);
-//   });
-//   promises = components.supplement.map(id => {
-//     return fetchSupplement(id);
-//   });
-//   results = await Promise.all(promises);
-//   results.forEach(result => {
-//     response.supplement.push(result.data);
-//   });
-//   return response;
-// }
-
-export function getPlateSize(plate) {
-  return {
-    rows: plate.wells.length,
-    columns: plate.wells[0].length,
-  };
-}
-
 export function getComponentCounts(plates) {
   const counts = {};
   const allWells = plates
-    .map(plate => {
+    .map((plate) => {
       return plate.wells.flat();
     })
     .flat();
-  allWells.forEach(well => {
-    well.components.forEach(component => {
+  allWells.forEach((well) => {
+    well.components.forEach((component) => {
       if (counts[component.id]) counts[component.id]++;
       else counts[component.id] = 1;
     });
@@ -349,7 +79,7 @@ export function getComponentCounts(plates) {
   return counts;
 }
 
-export const groupComponents = components => {
+export const groupComponents = (components) => {
   const groups = {
     communities: [],
     compounds: [],
@@ -357,7 +87,7 @@ export const groupComponents = components => {
     supplements: [],
     attributes: [],
   };
-  components.forEach(component => {
+  components.forEach((component) => {
     let key;
     if (component.type === COMPONENT_TYPE_COMMUNITY) {
       key = 'communities';
@@ -387,58 +117,4 @@ export function sortComponentsByType(components) {
   return arrayToSort.sort((a, b) => {
     return sortValues[a.type] - sortValues[b.type];
   });
-}
-
-export function getPlateStatistics(plate) {
-  const statistics = {
-    emptyBorders: true,
-    empty: true,
-    full: false,
-    numberOfWells: 0,
-    numberOfEmptyWells: 0,
-    numberOfFilledWells: 0,
-    maxComponentsInWell: 0,
-  };
-  const index = {
-    community: [],
-    compound: [],
-    medium: [],
-    supplement: [],
-    attribute: [],
-  };
-  plate.data.forEach((row, i) => {
-    row.forEach((well, j) => {
-      statistics.numberOfWells++;
-      if (well.components.length) {
-        statistics.empty = false;
-        statistics.numberOfFilledWells++;
-        const componentCount = well.components.length;
-        if (componentCount > statistics.maxComponentsInWell) {
-          statistics.maxComponentsInWell = componentCount;
-        }
-        if (i === 0 || i === plate.data.length - 1) {
-          statistics.emptyBorders = false;
-        } else if (j === 0 || j === row.length - 1) {
-          statistics.emptyBorders = false;
-        }
-        well.components.forEach(component => {
-          const typeArray = index[component.type];
-          if (!typeArray.includes(component.id)) {
-            typeArray.push(component.id);
-          }
-        });
-      } else {
-        statistics.numberOfEmptyWells++;
-      }
-    });
-  });
-  if (statistics.numberOfWells === statistics.numberOfFilledWells) {
-    statistics.full = true;
-  }
-  statistics.numberOfDistinctCommunities = index.community.length;
-  statistics.numberOfDistinctCompounds = index.compound.length;
-  statistics.numberOfDistinctMedia = index.medium.length;
-  statistics.numberOfDistinctSupplements = index.supplement.length;
-  statistics.numberOfDistinctAttributes = index.attribute.length;
-  return statistics;
 }
