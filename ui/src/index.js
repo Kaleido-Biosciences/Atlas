c78da38dd43f73ff395673c56cf33667b149889f
kvedananda <keith.vedananda@kaleido.com>
2020-04-17 19:14:20 -0400

    Rename and move kapture-backend -> KaptureApp

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './implementations/kapture-backend/App';
+import App from 'KaptureApp/App';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
