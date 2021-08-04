fb157ef6519d97344a279f8872451bd20fe6f73c
kvedananda <keith.vedananda@kaleido.com>
2021-08-03 23:25:18 -0400

    Add Inter font

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
