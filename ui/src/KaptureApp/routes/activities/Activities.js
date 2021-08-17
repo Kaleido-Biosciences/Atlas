6ebb3882ec01d6ec34166213f49c0543ec2e6c31
kvedananda <keith.vedananda@kaleido.com>
2021-08-17 17:53:43 -0400

    Replace activities route with barebones route for views

--- ui/src/KaptureApp/routes/activities/Activities.js
+++ ui/src/KaptureApp/routes/activities/Activities.js
@@ -1,232 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Loader, Message } from 'semantic-ui-react';
-import { Route, Switch, matchPath } from 'react-router-dom';
-
-import { PrintActions } from 'AtlasUI/components';
-import {
-  ActivityHeader,
-  ActivityDetails,
-  Editor,
-  EditorSidebar,
-  EditorActions,
-  Print,
-  GridTabs,
-  ActiveGrid,
-} from 'KaptureApp/components';
-import { CompletedModal } from './CompletedModal';
-import styles from './Activity.module.css';
+import { Activity } from 'KaptureApp/components';
 
 export class Activities extends Component {
-  state = {
-    modalOpen: false,
-    contentRef: null,
-  };
-  constructor(props) {
-    super(props);
-    this.loadActivity();
-  }
-  componentDidUpdate(prevProps, prevState) {
-    const { loading, error, containerCollectionsStale } = this.props;
-    if (
-      !loading &&
-      !error &&
-      containerCollectionsStale &&
-      this.matchDetailsPath()
-    ) {
-      this.loadActivity();
-    }
+  componentDidMount() {
+    const { activityId } = this.props.match.params;
+    this.props.onMount(activityId);
   }
   componentWillUnmount() {
     if (this.props.onUnmount) {
       this.props.onUnmount();
     }
   }
-  matchDetailsPath() {
-    const { path } = this.props.match;
-    const { pathname: url } = this.props.location;
-    const match = matchPath(url, {
-      path,
-      exact: true,
-      strict: false,
-    });
-    return match;
-  }
-  matchEditorPath() {
-    const path = this.props.match.path + '/editor';
-    const { pathname: url } = this.props.location;
-    const match = matchPath(url, {
-      path,
-      exact: true,
-      strict: false,
-    });
-    return match;
-  }
-  matchPrintPath() {
-    const path = this.props.match.path + '/print';
-    const { pathname: url } = this.props.location;
-    const match = matchPath(url, {
-      path,
-      exact: true,
-      strict: false,
-    });
-    return match;
-  }
-  loadActivity() {
-    const { activityId } = this.props.match.params;
-    this.props.loadActivity(activityId);
-  }
-  handleMarkAsCompleted = () => {
-    this.setState({ modalOpen: true });
-    if (this.props.onMarkAsCompleted) {
-      this.props.onMarkAsCompleted();
-    }
-  };
-  goToRoute = ({ route }) => {
-    let url = this.props.match.url;
-    url = url.endsWith('/') ? url.slice(0, -1) : url;
-    this.props.history.push(url + route);
-  };
-  goToActivityDetails = () => {
-    const { activityId } = this.props;
-    this.closeCompletedModal();
-    this.props.history.push(`/activities/${activityId}`);
-  };
-  goToActivity = (id) => {
-    this.props.history.push(`/activities/${id}`);
-    this.props.loadActivity(id);
-  };
-  goToPrint = () => {
-    const { publishedContainerCollectionDetails, activityId } = this.props;
-    const { status, version } = publishedContainerCollectionDetails;
-    const url = `/activities/${activityId}/print?status=${status}&version=${version}`;
-    this.closeCompletedModal();
-    this.props.history.push(url);
-  };
-  closeCompletedModal = () => {
-    this.setState({ modalOpen: false });
-    if (this.props.onCompletedModalClose) {
-      this.props.onCompletedModalClose();
-    }
-  };
   render() {
-    const {
-      initialized,
-      error,
-      loading,
-      match,
-      publishPending,
-      publishSuccess,
-      publishError,
-      containerCollectionsStale,
-      editorInitialized,
-      printInitialized,
-    } = this.props;
-    let content,
-      actions = null;
-    if (loading) {
-      content = (
-        <div className={styles.loader}>
-          <Loader active inline="centered">
-            Loading activity
-          </Loader>
-        </div>
-      );
-    } else if (error) {
-      content = (
-        <Message
-          negative
-          className={styles.errorMessage}
-          icon="warning circle"
-          header="An error occurred while loading the activity:"
-          content={error}
-        />
-      );
-    } else if (initialized) {
-      const tabsComponent = <GridTabs />;
-      const activeGridComponent = <ActiveGrid />;
-      const sidebarComponent = <EditorSidebar />;
-      if (this.matchEditorPath() && editorInitialized) {
-        actions = (
-          <EditorActions onMarkAsCompleted={this.handleMarkAsCompleted} />
-        );
-      } else if (this.matchPrintPath() && printInitialized) {
-        actions = <PrintActions contentRef={this.state.contentRef} />;
-      }
-      content = (
-        <React.Fragment>
-          <ActivityHeader
-            actions={actions}
-            onGoToActivityClick={this.goToActivity}
-          />
-          <Switch>
-            <Route
-              path={`${match.path}`}
-              exact
-              render={(routeProps) => {
-                return containerCollectionsStale ? null : (
-                  <ActivityDetails
-                    {...routeProps}
-                    onCollectionClick={this.goToRoute}
-                  />
-                );
-              }}
-            />
-            <Route
-              path={`${match.path}/editor`}
-              render={(routeProps) => {
-                return (
-                  <Editor
-                    tabsComponent={tabsComponent}
-                    activeGridComponent={activeGridComponent}
-                    sidebarComponent={sidebarComponent}
-                    {...routeProps}
-                  />
-                );
-              }}
-            />
-            <Route
-              path={`${match.path}/print`}
-              render={(routeProps) => (
-                <Print
-                  contentRef={(contentRef) => {
-                    this.setState({ contentRef });
-                  }}
-                  {...routeProps}
-                />
-              )}
-            />
-          </Switch>
-          <CompletedModal
-            open={this.state.modalOpen}
-            pending={publishPending}
-            success={publishSuccess}
-            error={publishError}
-            onBackToActivityClick={this.goToActivityDetails}
-            onPrintClick={this.goToPrint}
-          />
-        </React.Fragment>
-      );
+    let content;
+    if (this.props.loading) {
+      content = <div>Loading</div>;
+    } else if (this.props.initializationError) {
+      content = <div>Error</div>;
+    } else if (this.props.initialized) {
+      content = <Activity />;
     }
-    return <div className={styles.activity}>{content}</div>;
+    return <div>{content}</div>;
   }
 }
 
 Activities.propTypes = {
-  match: PropTypes.object.isRequired,
   initialized: PropTypes.bool,
-  error: PropTypes.string,
+  initializationError: PropTypes.string,
   loading: PropTypes.bool,
-  activityId: PropTypes.number,
-  publishPending: PropTypes.bool,
-  publishSuccess: PropTypes.bool,
-  publishError: PropTypes.string,
-  publishedContainerCollectionDetails: PropTypes.object,
-  containerCollectionsStale: PropTypes.bool,
-  editorInitialized: PropTypes.bool,
-  printInitialized: PropTypes.bool,
-  loadActivity: PropTypes.func.isRequired,
-  onMarkAsCompleted: PropTypes.func,
+  onMount: PropTypes.func,
   onUnmount: PropTypes.func,
-  onCompletedModalClose: PropTypes.func,
 };
--- ui/src/KaptureApp/routes/activities/connect.js
+++ ui/src/KaptureApp/routes/activities/connect.js
@@ -1,43 +1,18 @@
 import { connect } from 'react-redux';
-
 import { Activities } from './Activities';
-import { activity, editor, print } from 'KaptureApp/store';
+import { activity } from 'KaptureApp/store';
 
 const mapState = (state, props) => {
-  const initialized = activity.selectInitialized(state);
-  const error = activity.selectInitializationError(state);
-  let loading = false;
-  if (!initialized && !error) {
-    loading = true;
-  }
-  const publishSuccess = activity.selectPublishSuccess(state);
-  const publishError = activity.selectPublishError(state);
-  let publishPending = false;
-  if (!publishSuccess && !publishError) {
-    publishPending = true;
-  }
   return {
-    initialized,
-    loading,
-    error,
-    activityId: activity.selectId(state),
-    publishPending,
-    publishSuccess,
-    publishError,
-    publishedContainerCollectionDetails: activity.selectPublishedContainerCollectionDetails(
-      state
-    ),
-    containerCollectionsStale: activity.selectContainerCollectionsStale(state),
-    editorInitialized: editor.selectInitialized(state),
-    printInitialized: print.selectInitialized(state),
+    initialized: activity.selectInitialized(state),
+    initializationError: activity.selectInitializationError(state),
+    loading: activity.selectLoading(state),
   };
 };
 
 const mapDispatch = {
-  loadActivity: activity.loadActivity,
-  onMarkAsCompleted: activity.publishActivityGrids,
+  onMount: activity.loadActivity,
   onUnmount: activity.resetActivity,
-  onCompletedModalClose: activity.resetPublishState,
 };
 
 const connected = connect(mapState, mapDispatch)(Activities);
