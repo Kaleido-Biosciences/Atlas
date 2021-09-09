39218511ea270057864afb6f353f6a5ffb5bd53a
kvedananda <keith.vedananda@kaleido.com>
2021-09-09 02:08:56 -0400

    Display components in PlateTable

--- ui/src/KaptureApp/components/activity/PlateTable/PlateTable.js
+++ ui/src/KaptureApp/components/activity/PlateTable/PlateTable.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { ContainerComponent } from 'AtlasUI/components';
+import styles from './PlateTable.module.css';
 
 export class PlateTable extends Component {
+  renderComponents(position) {
+    if (position.container) {
+      return position.container.components.map((component) => {
+        return (
+          <div className={styles.containerComponent}>
+            <ContainerComponent
+              component={component}
+              enableRemove={this.props.enableRemoveComponent}
+              key={component.id}
+              onRemove={this.props.onRemoveComponent}
+              position={position}
+            />
+          </div>
+        );
+      });
+    } else return null;
+  }
   renderPlate() {
     const plate = this.props.view.data.grids[0];
-    return plate.data.map((well, i) => {
+    return plate.positions.map((position, i) => {
       return (
-        <tr key={well.name} className={i % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
+        <tr
+          key={position.name}
+          className={i % 2 === 0 ? 'bg-white' : 'bg-gray-50'}
+        >
           <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-            {well.name}
+            {position.name}
           </td>
-          <td></td>
+          <td className="px-6 py-4 text-sm text-gray-900">
+            <div className={styles.components}>
+              {this.renderComponents(position)}
+            </div>
+          </td>
         </tr>
       );
     });
