554ef0813e20f7bd4c7c32f3236d59b9db1bdb06
kvedananda <keith.vedananda@kaleido.com>
2021-09-15 12:57:18 -0400

    Display wells in overview grid

--- ui/src/KaptureApp/components/activity/Overview/Grid.js
+++ ui/src/KaptureApp/components/activity/Overview/Grid.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './Grid.module.css';
 import classNames from 'classnames';
 import { EditableText } from '../../EditableText';
+import { getGridRows } from 'AtlasUI/utils/grid';
 
 export class Grid extends Component {
   handleClick = () => {
@@ -15,24 +16,39 @@ export class Grid extends Component {
       this.props.onSaveName(this.props.grid.id, value);
     }
   };
+  renderGrid() {
+    const rows = getGridRows(this.props.grid);
+    const renderedRows = rows.map((row) => {
+      const positions = row.map((position) => {
+        return (
+          <div
+            className={classNames(styles.container, 'border-gray-400', {
+              'bg-gray-400': position.container.components.length > 0,
+            })}
+          ></div>
+        );
+      });
+      return <div className={styles.row}>{positions}</div>;
+    });
+    return <div>{renderedRows}</div>;
+  }
   render() {
     const className = classNames(
       styles.grid,
       'border',
       'border-gray-300',
-      'p-1',
       'text-xs',
       'cursor-pointer'
     );
     return (
       <div className={className} onClick={this.handleClick}>
-        <div className="text-xs">
+        <div className="text-xxs font-bold">
           <EditableText
             onSave={this.handleSaveName}
             value={this.props.grid.name}
           />
         </div>
-        <div className="text-gray-400">{`${this.props.grid.positions.length} wells`}</div>
+        <div className={styles.gridContainer}>{this.renderGrid()}</div>
       </div>
     );
   }
