2807ee02596b43cb641889a7667503f3a3ade5ab
kvedananda <keith.vedananda@kaleido.com>
2021-09-20 18:15:40 -0400

    Pass extra props into button

--- ui/src/KaptureApp/components/Button.js
+++ ui/src/KaptureApp/components/Button.js
@@ -4,23 +4,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export class Button extends Component {
   render() {
-    let className;
-    if (this.props.secondary) {
-      className =
+    const { className, content, icon, secondary, ...props } = this.props;
+    let buttonClasses;
+    if (secondary) {
+      buttonClasses =
         'inline-flex items-center px-2.5 py-1.5 border border-gray-300  text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none';
     } else {
-      className =
+      buttonClasses =
         'inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded  text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none';
     }
-    if (this.props.className) {
-      className = className + ' ' + this.props.className;
+    if (className) {
+      buttonClasses = buttonClasses + ' ' + className;
     }
     return (
-      <button onClick={this.props.onClick} type="button" className={className}>
-        {this.props.icon ? (
-          <FontAwesomeIcon className="mr-1" icon={this.props.icon} />
-        ) : null}
-        {this.props.content}
+      <button type="button" className={buttonClasses} {...props}>
+        {icon ? <FontAwesomeIcon className="mr-1" icon={icon} /> : null}
+        {content}
       </button>
     );
   }
