6c74386e1139471dea58979deff1885da6fa8d07
kvedananda <keith.vedananda@kaleido.com>
2021-09-22 19:13:54 -0400

    Add Button disabled style

--- ui/src/KaptureApp/components/Button.js
+++ ui/src/KaptureApp/components/Button.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+const commonClasses =
+  'inline-flex items-center px-2.5 py-1.5 border text-xs font-medium rounded focus:outline-none';
+
 export class Button extends Component {
   render() {
-    const { className, content, icon, secondary, ...props } = this.props;
-    let buttonClasses;
+    const { className, icon, secondary, ...props } = this.props;
+    const content = this.props.content || this.props.children;
+    let buttonClasses = commonClasses;
     if (secondary) {
-      buttonClasses =
-        'inline-flex items-center px-2.5 py-1.5 border border-gray-300  text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none';
+      buttonClasses += ' border-gray-300 text-gray-700 bg-white';
+      if (!this.props.disabled) buttonClasses += ' hover:bg-gray-50';
     } else {
-      buttonClasses =
-        'inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded  text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none';
+      buttonClasses += ' border-transparent text-white bg-indigo-600';
+      if (!this.props.disabled) buttonClasses += ' hover:bg-indigo-700';
+    }
+    if (this.props.disabled) {
+      buttonClasses += ' disabled:opacity-50';
     }
     if (className) {
-      buttonClasses = buttonClasses + ' ' + className;
+      buttonClasses += ' ' + className;
     }
     return (
       <button type="button" className={buttonClasses} {...props}>
@@ -26,7 +33,7 @@ export class Button extends Component {
 }
 
 Button.propTypes = {
-  content: PropTypes.string.isRequired,
+  content: PropTypes.string,
   icon: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
   secondary: PropTypes.bool,
 };
