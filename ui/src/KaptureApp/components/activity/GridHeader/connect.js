21a3e8de429fba12aa3dc2e1b591ecda854b0655
kvedananda <keith.vedananda@kaleido.com>
2021-09-29 12:40:02 -0400

    Remove GridHeader

--- ui/src/KaptureApp/components/activity/GridHeader/connect.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import { GridHeader } from './GridHeader';
-import { activity } from 'KaptureApp/store';
-
-const mapDispatch = { onSaveName: activity.setGridName };
-
-const connected = connect(null, mapDispatch)(GridHeader);
-export { connected as GridHeader };
