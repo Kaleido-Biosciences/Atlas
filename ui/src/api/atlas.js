797c6a98599764ccec44b39f4c50d0aeccb977af
kvedananda <keith.vedananda@kaleido.com>
2021-10-20 17:40:21 -0400

    Remove plate sort and positioning

--- ui/src/api/atlas.js
+++ ui/src/api/atlas.js
@@ -13,23 +13,6 @@ export async function fetchActivity(id) {
   const { platemaps: plateMaps, ...rest } = activityData;
   const activity = { ...rest, plates: [] };
   if (plateMaps) {
-    plateMaps.sort((a, b) => {
-      if (a.barcode === b.barcode) return 0;
-      else if (a.barcode < b.barcode) return -1;
-      else return 1;
-    });
-    let unpositionedCount = 0;
-    plateMaps.forEach((plateMap, i) => {
-      if (
-        isNaN(plateMap.overviewPositionTop) &&
-        isNaN(plateMap.overviewPositionLeft)
-      ) {
-        plateMap.overviewPositionTop = (unpositionedCount + 1) * 10;
-        plateMap.overviewPositionLeft = 10;
-        unpositionedCount++;
-      }
-      plateMap.plateNumber = i + 1;
-    });
     activity.plates = plateMaps.map((plateMap) => createPlate(plateMap));
   }
   return activity;
