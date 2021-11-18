f3b69cb54b0a03c29595c581013b70c86a12e0e9
kvedananda <keith.vedananda@kaleido.com>
2021-11-18 17:11:14 -0500

    Remove findComponent

--- ui/src/api/kapture.js
+++ ui/src/api/kapture.js
@@ -1,13 +1,6 @@
 import axios from 'axios';
 
 import { API_URL } from 'KaptureApp/config/api';
-import {
-  COMPONENT_TYPE_COMMUNITY,
-  COMPONENT_TYPE_COMPOUND,
-  COMPONENT_TYPE_MEDIUM,
-  COMPONENT_TYPE_SUPPLEMENT,
-  COMPONENT_TYPE_ATTRIBUTE,
-} from 'KaptureApp/config/componentTypes';
 
 export function fetchCommunity(id) {
   return axios.get(API_URL + '/communities/' + id);
@@ -55,40 +48,4 @@ export function fetchSupplementByName(name) {
 
 export function searchSupplements(searchTerm) {
   return axios.get(API_URL + '/_search/supplements?size=5&query=' + searchTerm);
-}
-
-export function findComponent(name) {
-  const queryString = `?name.equals=${name}`;
-  const communities = axios.get(API_URL + '/communities' + queryString);
-  const compounds = axios.get(API_URL + '/batches' + queryString);
-  const media = axios.get(API_URL + '/media' + queryString);
-  const supplements = axios.get(API_URL + '/supplements' + queryString);
-  return Promise.all([communities, compounds, media, supplements]).then(
-    (response) => {
-      const result = {
-        name,
-        found: false,
-        type: null,
-        data: null,
-      };
-      if (response[0].data.length) {
-        result.found = true;
-        result.type = COMPONENT_TYPE_COMMUNITY;
-        result.data = response[0].data[0];
-      } else if (response[1].data.length) {
-        result.found = true;
-        result.type = COMPONENT_TYPE_COMPOUND;
-        result.data = response[1].data[0];
-      } else if (response[2].data.length) {
-        result.found = true;
-        result.type = COMPONENT_TYPE_MEDIUM;
-        result.data = response[2].data[0];
-      } else if (response[3].data.length) {
-        result.found = true;
-        result.type = COMPONENT_TYPE_SUPPLEMENT;
-        result.data = response[3].data[0];
-      }
-      return result;
-    }
-  );
 }
