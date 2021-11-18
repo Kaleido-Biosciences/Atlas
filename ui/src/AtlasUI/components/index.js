a7b8253c6b9489848c0678bb56f325b54eb6a8b4
kvedananda <keith.vedananda@kaleido.com>
2021-11-18 13:28:20 -0500

    Remove ComponentTypeCircle

--- ui/src/AtlasUI/components/index.js
+++ ui/src/AtlasUI/components/index.js
@@ -1,5 +1,4 @@
 export { Print } from './print/Print';
 export { PrintActions } from './print/PrintActions';
-export { ComponentTypeCircle } from './editor/ComponentTypeCircle';
 export { ContainerComponent } from './ContainerComponent';
 export { ComponentTooltip } from './ComponentTooltip';
