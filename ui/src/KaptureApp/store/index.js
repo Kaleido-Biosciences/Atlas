1959f24c5ec86375e718238ebb01d274d153552f
kvedananda <keith.vedananda@kaleido.com>
2021-11-18 17:10:15 -0500

    Remove components slice

--- ui/src/KaptureApp/store/index.js
+++ ui/src/KaptureApp/store/index.js
@@ -1,7 +1,6 @@
 export { store } from './store';
 export { activity } from './activity';
 export { activitySearch } from './activitySearch';
-export { components } from './components';
 export { editorImport } from './editorImport';
 export { print } from './print';
 export { tools } from './tools';
--- ui/src/KaptureApp/store/store.js
+++ ui/src/KaptureApp/store/store.js
@@ -3,7 +3,6 @@ import logger from 'redux-logger';
 
 import { activity } from './activity';
 import { activitySearch } from './activitySearch';
-import { components } from './components';
 import { print } from './print';
 import { tools } from './tools';
 import { editorImport } from './editorImport';
@@ -12,7 +11,6 @@ export const store = configureStore({
   reducer: {
     activity: activity.reducer,
     activitySearch: activitySearch.reducer,
-    components: components.reducer,
     editorImport: editorImport.reducer,
     tools: tools.reducer,
     print: print.reducer,
