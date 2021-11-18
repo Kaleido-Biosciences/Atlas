460b59b7ce5d9dff23e79d37bd03fa9ae55b19cf
kvedananda <keith.vedananda@kaleido.com>
2021-11-18 13:13:10 -0500

    Remove Editor

--- ui/src/AtlasUI/components/index.js
+++ ui/src/AtlasUI/components/index.js
@@ -1,7 +1,6 @@
 export { Print } from './print/Print';
 export { PrintActions } from './print/PrintActions';
 export { ComponentTypeCircle } from './editor/ComponentTypeCircle';
-export { Editor } from './editor/Editor';
 export { GridTabs } from './editor/GridTabs';
 export { ActiveGrid } from './editor/ActiveGrid';
 export { Timepoint } from './editor/Timepoint';
