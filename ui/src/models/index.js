b23beafb774e950db92cef75c0755917042e8712
kvedananda <keith.vedananda@kaleido.com>
2021-11-18 17:29:16 -0500

    Remove grid model

--- ui/src/models/index.js
+++ ui/src/models/index.js
@@ -1,12 +1,5 @@
 export { createContainer } from './container';
 export {
-  createGrid,
-  createGridPositions,
-  createContainersForGrid,
-  createGridPosition,
-  addContainersToGrid,
-} from './grid';
-export {
   createPlate,
   createWells,
   createRowHeaders,
