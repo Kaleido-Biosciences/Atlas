0853b7d5bdcecbf34a8dcd60a7075538f46813fd
kvedananda <keith.vedananda@kaleido.com>
2021-11-18 13:16:24 -0500

    Remove ActiveGrid

--- ui/src/AtlasUI/components/index.js
+++ ui/src/AtlasUI/components/index.js
@@ -2,7 +2,6 @@ export { Print } from './print/Print';
 export { PrintActions } from './print/PrintActions';
 export { ComponentTypeCircle } from './editor/ComponentTypeCircle';
 export { GridTabs } from './editor/GridTabs';
-export { ActiveGrid } from './editor/ActiveGrid';
 export { Timepoint } from './editor/Timepoint';
 export { Grid } from './editor/Grid';
 export { ContainerComponent } from './ContainerComponent';
