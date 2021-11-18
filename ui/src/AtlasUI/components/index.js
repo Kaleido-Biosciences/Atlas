860962a6d2b3154aed729324adb2d11d6a492bcd
kvedananda <keith.vedananda@kaleido.com>
2021-11-18 13:22:05 -0500

    Remove Grid

--- ui/src/AtlasUI/components/index.js
+++ ui/src/AtlasUI/components/index.js
@@ -2,6 +2,5 @@ export { Print } from './print/Print';
 export { PrintActions } from './print/PrintActions';
 export { ComponentTypeCircle } from './editor/ComponentTypeCircle';
 export { Timepoint } from './editor/Timepoint';
-export { Grid } from './editor/Grid';
 export { ContainerComponent } from './ContainerComponent';
 export { ComponentTooltip } from './ComponentTooltip';
