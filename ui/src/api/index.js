69cd07bfa29d79e1d18cfc6b08e5bf13b7eb2215
kvedananda <keith.vedananda@kaleido.com>
2021-11-18 16:16:31 -0500

    Remove exportGrids

--- ui/src/api/index.js
+++ ui/src/api/index.js
@@ -1,11 +1,9 @@
 import * as kapture from './kapture';
 import * as aws from './aws';
 import * as atlas from './atlas';
-import { exportGrids } from './exportGrids';
 
 export const api = {
   ...aws,
   ...kapture,
   ...atlas,
-  exportGrids,
 };
