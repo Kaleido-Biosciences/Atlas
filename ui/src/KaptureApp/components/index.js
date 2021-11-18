1e85fdeb724c1b9d9c015cd0853abec743c30d4c
kvedananda <keith.vedananda@kaleido.com>
2021-11-18 13:04:01 -0500

    Remove EditorActions

--- ui/src/KaptureApp/components/index.js
+++ ui/src/KaptureApp/components/index.js
@@ -1,5 +1,4 @@
 export { Header } from './Header';
-export { EditorActions } from './EditorActions';
 export { Activity } from './activity/Activity';
 export { Button } from './ui/Button';
 export { ButtonGroup } from './ui/ButtonGroup';
