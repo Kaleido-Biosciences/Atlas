0d6a582c7bde92723e8548857736a69a8771091c
kvedananda <keith.vedananda@kaleido.com>
2021-11-18 13:25:25 -0500

    Remove Timepoint

--- ui/src/AtlasUI/components/index.js
+++ ui/src/AtlasUI/components/index.js
@@ -1,6 +1,5 @@
 export { Print } from './print/Print';
 export { PrintActions } from './print/PrintActions';
 export { ComponentTypeCircle } from './editor/ComponentTypeCircle';
-export { Timepoint } from './editor/Timepoint';
 export { ContainerComponent } from './ContainerComponent';
 export { ComponentTooltip } from './ComponentTooltip';
