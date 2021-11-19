5937725916fc9d3512909942786d24806b0fde5b
kvedananda <keith.vedananda@kaleido.com>
2021-11-19 00:02:22 -0500

    Move App up to src

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { store } from 'store';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './fontAwesome';
-import { version } from '../../package.json';
+import { version } from '../package.json';
 import { Header } from 'KaptureApp/components';
-import { store } from 'store';
 import { Home, Activities } from 'routes';
 import styles from './App.module.css';
 
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from 'KaptureApp/App';
+import App from './App';
 import './index.css';
 //import reportWebVitals from './reportWebVitals';
 
