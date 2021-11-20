528b86f6a6587a66cb723255d30635fef82ec322
kvedananda <keith.vedananda@kaleido.com>
2021-11-19 19:24:11 -0500

    Move api config

--- ui/src/api/atlas.js
+++ ui/src/api/atlas.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { API_URL } from 'KaptureApp/config/api';
+import { API_URL } from 'config/api';
 import { exportComponent } from 'KaptureApp/config/componentTypes';
 
 export async function searchActivities(searchTerm) {
